0673e952479a522b54decf680dbb1ee9eed8ab16
KBellaBlue <kybfreeman@gmail.com>
2022-10-31 08:57:19 -0700

    fixed return number output including line ending character

--- main-tests/CS_known_main.c
+++ main-tests/CS_known_main.c
@@ -6,14 +6,14 @@ int main(void)
 	int retVal = 0;
 	int retVal2 = 0;
 
-	retVal = _printf("Ours: %", "test");
-	retVal2 = printf("Reg: %", "test");
-	printf("\n%d%d\n", retVal, retVal2);
-	retVal = _printf("Ours: %s", NULL);
-	retVal2 = printf("Reg: %s", NULL);
-	printf("\n%d%d\n", retVal, retVal2);
+	retVal = _printf("Ours: %\n", "test");
+	retVal2 = printf("Reg: %\n", "test");
+	printf("\nOurs: %d \n Reg: %d\n", retVal, retVal2);
+	retVal = _printf("Ours: %s\n", NULL);
+	retVal2 = printf("Reg: %s\n", NULL);
+	printf("\nOurs: %d \n Reg: %d\n", retVal, retVal2);
 	retVal = _printf(NULL, "test");
 	retVal2 = printf(NULL, "test");
-	printf("\n%d%d\n", retVal, retVal2);
+	printf("\n Ours: %d\n Reg: %d\n", retVal, retVal2);
 	return (0);
 }
