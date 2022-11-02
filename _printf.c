3f8ddc7c71ff0609ddbe03f58c62340a33845ec1
Julia-5534 <5534@holbertonstudents.com>
2022-11-01 23:18:51 -0700

    Making Betty happy

--- _printf.c
+++ _printf.c
@@ -18,45 +18,33 @@ int _printf(const char *format, ...)
 
 	if (format == NULL || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-
 	va_start(arg_list, format);
-	char_chars = 0;
-	while (format && format[i])
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{ /*check the operator*/
-			i++;
 			if (format[i] == '%')
-			{
 				char_chars += print_percent(arg_list);
-				i++;
-			}
 			else
 			{
 				p = 0;
 				while (p < 5)
 				{
 					if (f_list[p].op[0] == format[i])
-					{
 						char_chars += f_list[p].f(arg_list);
-						break;
-					}
-					else
-						p++;
+					p++;
 				}
 				if (p >= 5)
 				{
 					char_chars += _write_char(format[i - 1]);
 					char_chars += _write_char(format[i]);
 				}
-				i++;
 			}
-		}
-		else
-		{
-			char_chars += _write_char(format[i]);
 			i++;
 		}
+		else
+			char_chars += _write_char(format[i]);
+		i++;
 	}
 	va_end(arg_list);
 	return (char_chars);
