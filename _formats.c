ab3fbf6a572730efc46e6888a7ee7748d34c0bbb
Julia-5534 <5534@holbertonstudents.com>
2022-11-02 00:02:51 -0700

    Adjusting and checking print_integer

--- _formats.c
+++ _formats.c
@@ -56,12 +56,11 @@ int print_percent(__attribute__((unused))va_list list)
  */
 int print_integer(va_list list)
 {
-	int num_length;
+	int num_length = 0;
 
-	num_length = print_integer(list);
+	num_length += print_integer(list);
 
 	return (num_length);
-}
 
 /**
  * unsigned_integer - Prints Unsigned integers
